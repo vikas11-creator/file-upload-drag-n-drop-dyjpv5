b7d663914b4502cfc1dcdfe7c44654a6b86a4c77
vikas11-creator <82199478+vikas11-creator@users.noreply.github.com>
2023-04-12 16:01:04 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# file-upload-drag-n-drop-dyjpv5
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/file-upload-drag-n-drop-dyjpv5)
---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { HelloComponent } from './hello.component';
+import { FileDragNDropDirective } from './file-drag-n-drop.directive';
+
+import {MatIconModule} from '@angular/material/icon';
+import {MatInputModule} from '@angular/material/input';
+import {MatListModule} from '@angular/material/list';
+import {MatButtonModule} from '@angular/material/button';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { DialogConfirmComponent } from './dialog-confirm/dialog-confirm.component';
+
+import {MatDialogModule} from '@angular/material/dialog';
+
+@NgModule({
+  imports:      [ BrowserModule, FormsModule, MatIconModule, MatInputModule, MatListModule, MatButtonModule, MatGridListModule, MatSnackBarModule, BrowserAnimationsModule, MatDialogModule ],
+  declarations: [ AppComponent, HelloComponent, FileDragNDropDirective, DialogConfirmComponent ],
+  bootstrap:    [ AppComponent ],
+  entryComponents: [ DialogConfirmComponent ]
+})
+export class AppModule { }
